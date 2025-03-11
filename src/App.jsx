02cf15485c1ec2fa631fb55276ff26ccc8c053fb
yasiralakus <yasiralakus11@gmail.com>
2025-03-11 17:27:43 +0300

    project completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+
+    return (
+        <div className="full-page">
+
+            <div className="main-box">
+
+                <img src="./images/image-qr-code.png" alt="" />
+
+                <h1>Improve your front-end skills by building projects</h1>
+
+                <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+
+            </div>
+
+        </div>
+    )
+}
